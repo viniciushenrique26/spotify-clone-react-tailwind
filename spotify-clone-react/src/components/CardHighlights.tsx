77b1fbbcbf795559a03a8d543b5ce9a4222137e9
viniciushenrique26 <viniciusslavov@gmail.com>
2024-02-15 19:37:49 -0300

    fix: update props CardHighlights

--- spotify-clone-react/src/components/CardHighlights.tsx
+++ spotify-clone-react/src/components/CardHighlights.tsx
@@ -1,15 +1,22 @@
-import PlayIcon from '../assets/playIcon.svg'
-export function CardHighlight(){
+import PlayIcon from '../assets/playIcon.svg' 
+
+
+interface CardHighlightProps { 
+    img: string;
+    title: string;
+    artist: string;
+}
+export function CardHighlight(props: CardHighlightProps){
     return ( 
         <div className="p-4  bg-zinc-900 flex flex-col gap-2 h-64 rounded-lg group relative hover:bg-zinc-800 transition"> 
             
-                <img src="https://misc.scdn.co/liked-songs/liked-songs-64.png" alt="album" className="w-36 object-cover rounded-md "/>  
+                <img src={props.img} alt="album" className="w-40 object-cover rounded-md "/>  
                 <button className='absolute top-28 right-0 w-8 h-8 shadow-xl flex items-center justify-center pr-1 p-2 rounded-full bg-green-400 text-black ml-auto mr-6 invisible group-hover:visible group-hover:animate-fade'>
                     <img src={PlayIcon} className='size-4'alt="" />
                 </button>
             
-            <p className="text-sm font-semibold text-zinc-100">Title</p>  
-            <p className="text-xs font-medium text-zinc-500">Artista</p> 
+            <p className="text-sm font-semibold text-zinc-100 text-ellipsis">{props.title}</p>  
+            <p className="text-xs font-medium text-zinc-500">{props.artist}</p> 
             
         </div>
     )
--- spotify-clone-react/src/components/Highlights.tsx
+++ spotify-clone-react/src/components/Highlights.tsx
@@ -1,14 +1,46 @@
 import { CardHighlight } from "./CardHighlights";
 
+
 export function Highlights (){
     return (
-        <div className='flex flex-col gap-2'>
-           <h4 className='font-semibold text-xl hover:underline'>Parecido com Arlindo Cruz</h4> 
-            <div className='flex gap-4'>
-                <CardHighlight/> 
-                <CardHighlight/>
-                <CardHighlight/> 
-                <CardHighlight/>
+        <div className='flex flex-col gap-2'> 
+            <div className="flex items-center justify-between">
+                <h4 className='font-semibold text-xl hover:underline'>Parecido com Arlindo Cruz</h4>  
+           
+                <a href="" className="text-xs font-semibold text-zinc-500 hover:underline">Mostrar tudo</a>
+           
+            </div>
+            <div className='flex gap-6'>
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Zeca Pagodinho"
+                    artist="Zeca Pagodinho"
+                /> 
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Pagode das antigas"
+                    artist="Vários artistas"
+                />  
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Pagode das antigas"
+                    artist="Vários artistas"
+                /> 
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Pagode das antigas"
+                    artist="Vários artistas"
+                /> 
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Pagode das antigas"
+                    artist="Vários artistas"
+                /> 
+                <CardHighlight 
+                    img="https://misc.scdn.co/liked-songs/liked-songs-64.png"
+                    title="Pagode das antigas"
+                    artist="Vários artistas"
+                />
             </div>
         </div> 
     )
